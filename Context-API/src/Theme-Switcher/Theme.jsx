dd393a13e545d00b6fde269c45c959c474bcda24
Suhana783 <suhanachauhan133@gmail.com>
2025-11-12 11:33:03 +0530

    added basic css

--- Context-API/src/Theme-Switcher/Theme.jsx
+++ Context-API/src/Theme-Switcher/Theme.jsx
@@ -1,5 +1,6 @@
 import React, {useContext, useEffect} from 'react';
 import { ThemeContext } from './ThemeC.jsx';
+import './Theme-style.css';
 
 function ThemeChanger () {
     const {theme, Toggletheme, color} = useContext(ThemeContext);
